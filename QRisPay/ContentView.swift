323fbcb270954cb562b26431787de8e57a4cfdcb
oreki9 <44063419+oreki9@users.noreply.github.com>
2024-02-02 21:40:29 +0700

    Initial Commit

---
+++ QRisPay/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  QRisPay
+//
+//  Created by rickyTA on 02/02/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ QRisPay/QRisPayApp.swift
@@ -0,0 +1,17 @@
+//
+//  QRisPayApp.swift
+//  QRisPay
+//
+//  Created by rickyTA on 02/02/24.
+//
+
+import SwiftUI
+
+@main
+struct QRisPayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
