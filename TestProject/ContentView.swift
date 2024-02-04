02635a7f4d5dff041cdc5e5140f85e46742907c7
oreki9 <44063419+oreki9@users.noreply.github.com>
2024-02-04 18:43:30 +0700

    add more massage

--- TestProject/ContentView.swift
+++ TestProject/ContentView.swift
@@ -7,7 +7,7 @@
 // Case: Aplikasi Mobile App Pembayaran QRIS is branch main (need more polish)
 // Case: Aplikasi Mobile App Promo is branch HitProject (need more polish)
 // Case: Aplikasi Mobile App Portfolio is branch chartTest (its not done)
-// im sorry but it seems i cant finish the test because i dont have time for it
+// im sorry but it seems i cant finish the test because i dont have time for it, and i think i had enough of it, im sorry if i waste your time
 import SwiftUI
 
 struct ContentView: View {
